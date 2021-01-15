d8372318b6bf0c5afcfc025a7a5fc941fa67078a
sindy964 <herrera.sindy1993@gmail.com>
2021-01-15 16:19:22 -0600

    Segunda entrega

--- clases/Config.php
+++ clases/Config.php
@@ -8,9 +8,9 @@ abstract class Config {
 //    protected $esquema= "mini_sedi";
 
     protected $servidor = "localhost";
-    protected $base_datos = "sigefi3";
-    protected $usuario = "postgres";
-    protected $contrasena = "ministerio"; //ojo la misma del rol mini_sedi_login
+    protected $base_datos = "sigefi";
+    protected $usuario = "mini_sedi_login";
+    protected $contrasena = "(m1n1*cd1)"; //ojo la misma del rol mini_sedi_login
     protected $puerto="5432";
     protected $esquema= "mini_sedi";
     
